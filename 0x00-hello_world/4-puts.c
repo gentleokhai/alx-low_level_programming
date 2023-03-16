69cb3c279c2caee1ec97355259e1ca23d6a4cbd6
gentleokhai <gentleokhaiX@gmail.com>
2023-03-16 08:02:44 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints a string using puts
- * return - 0(success)
+ * main : prints a string 
+ * return : 0 -success
  */
- int main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
