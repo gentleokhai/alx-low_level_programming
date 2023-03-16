bca922b81f60f2f47ce7a18ae7d750cc28d6e377
gentleokhai <gentleokhaiX@gmail.com>
2023-03-16 15:36:56 -0700

    ohk

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print a string followed by a new line
+ * return - 0 (sucess)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
