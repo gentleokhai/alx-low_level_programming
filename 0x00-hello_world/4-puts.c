d09b26760bfb7cd98be006cd0d3e27db886a9585
gentleokhai <gentleokhaiX@gmail.com>
2023-03-16 17:26:29 -0700

    wahalur

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
- * main : prints a string 
- * return : 0 -success
+ * main : Entry
+ * Return: 0 (sucess)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
