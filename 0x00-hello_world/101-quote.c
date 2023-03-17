9d671e3a8a65986b17450a5cd74e100b72eb584e
gentleokhai <gentleokhaiX@gmail.com>
2023-03-16 18:57:45 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - print a string followed by a new line
- * Return: 1 (sucess)
+ * main - Entry
+ * Return: 1
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
