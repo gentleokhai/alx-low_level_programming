858bfec3d46fc589293d5c1e43317b4cfbc765aa
gentleokhai <gentleokhaiX@gmail.com>
2023-03-30 09:20:18 -0700

    pointers, arrays, strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - Concentrate two strings
+ * @dest: Pointer to destination string.
+ * @src: Pointer to source string
+ * Return: Pointer to destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *start = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (start);
+}
