5918a6ab2d2b5d7e625886814c3eb24b18efa67e
gentleokhai <gentleokhaiX@gmail.com>
2023-04-19 16:39:24 -0700

    &pointers||arrays

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,21 +1,33 @@
-#include "function_pointers.h"
-
+#include"3-calc.h"
 /**
- * int_index - searches for an integer
- *
- * @array: pointer to array
- * @size: size of array
- * @cmp: function to compare and return match if it exists
- * Return: index of first match or -1 if not match or fail
- */
-int int_index(int *array, int size, int (*cmp)(int))
+ * get_op_func - get the operator and redirect to the func.
+ * @s: the operator
+(* a blank line
+* Description: get the operator and redirect to the func.)?
+(* section header: 3-calc.h)*
+* Return: return null or the function to do.
+*/
+
+int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 	int i;
 
-	if (array && cmp && size > 0)
-		for (i = 0; i < size; i++)
-			if (cmp(array[i]))
-				return (i);
-
-	return (-1);
+	i = 0;
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (*(ops[i]).f);
+		}
+		i++;
+	}
+	return (NULL);
 }
